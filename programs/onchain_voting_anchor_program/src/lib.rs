2d0514ddd6047a7deee31982946cea16fb7868d1
cihancanayyildiz <cihancanayyildiz@gmail.com>
2022-10-02 15:31:26 +0300

    Anchor program and unit tests completed.

--- programs/onchain_voting_anchor_program/src/lib.rs
+++ programs/onchain_voting_anchor_program/src/lib.rs
@@ -1,15 +1,52 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("8yrgysTNYEXkd7g78wDFnPAusft4cRxwiUrMetfFcevt");
 
 #[program]
 pub mod onchain_voting_anchor_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, description: String) -> Result<()> {
+        ctx.accounts.vote_account.is_open_to_vote = true;
+        ctx.accounts.vote_account.description = description;
+        Ok(())
+    }
+
+    pub fn give_vote(ctx: Context<GiveVote>, vote: String) -> Result<()> {
+        if vote == "YES" {
+            msg!("Voted for YES");
+            ctx.accounts.vote_account.yes += 1;
+        } else if vote == "NO" {
+            msg!("Voted for NO");
+            ctx.accounts.vote_account.no += 1;
+        } else {
+            return Err(Error::from(ProgramError::InvalidArgument));
+        }
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8 + 1 + 8 + 8 + 24)]
+    pub vote_account: Account<'info, Proposal>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct Proposal {
+    is_open_to_vote: bool,
+    description: String,
+    yes: u64,
+    no: u64,
+}
+
+#[derive(Accounts)]
+pub struct GiveVote<'info> {
+    #[account(mut)]
+    pub vote_account: Account<'info, Proposal>,
+    pub signer: Signer<'info>,
+}
--- tests/onchain_voting_anchor_program.ts
+++ tests/onchain_voting_anchor_program.ts
@@ -3,14 +3,56 @@ import { Program } from "@project-serum/anchor";
 import { OnchainVotingAnchorProgram } from "../target/types/onchain_voting_anchor_program";
 
 describe("onchain_voting_anchor_program", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    // Configure the client to use the local cluster.
+    anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.OnchainVotingAnchorProgram as Program<OnchainVotingAnchorProgram>;
+    const program = anchor.workspace
+        .OnchainVotingAnchorProgram as Program<OnchainVotingAnchorProgram>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+    let voteBank = anchor.web3.Keypair.generate();
+
+    it("Creating proposal!", async () => {
+        const tx = await program.methods
+            .initialize("Proposal description")
+            .accounts({
+                voteAccount: voteBank.publicKey,
+            })
+            .signers([voteBank])
+            .rpc();
+        console.log("Your transaction signature", tx);
+    });
+
+    it("Voting for yes.", async () => {
+        const tx = await program.methods
+            .giveVote("YES")
+            .accounts({
+                voteAccount: voteBank.publicKey,
+            })
+            .rpc();
+
+        console.log("Your transaction signature", tx);
+
+        let voteBankData = await program.account.proposal.fetch(
+            voteBank.publicKey
+        );
+        console.log(`Total Yes count: ${voteBankData.yes}`);
+        console.log(`Total No count: ${voteBankData.no}`);
+    });
+
+    it("Voting for no.", async () => {
+        const tx = await program.methods
+            .giveVote("NO")
+            .accounts({
+                voteAccount: voteBank.publicKey,
+            })
+            .rpc();
+
+        console.log("Your transaction signature", tx);
+
+        let voteBankData = await program.account.proposal.fetch(
+            voteBank.publicKey
+        );
+        console.log(`Total Yes count: ${voteBankData.yes}`);
+        console.log(`Total No count: ${voteBankData.no}`);
+    });
 });
